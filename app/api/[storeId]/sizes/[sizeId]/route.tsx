f475d4070380e9989c0877bf5e451a9e41da1aaf
Claudio Lins <claudio.lins@me.com>
2023-07-26 21:30:30 +0100

    06:05 => Created Sizes

---
+++ app/api/[storeId]/sizes/[sizeId]/route.tsx
@@ -0,0 +1,116 @@
+import prismaDb from "@/lib/prismaDb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { sizeId: string } }
+) {
+  try {
+    if (!params.sizeId) {
+      return new NextResponse("Size Id is required", { status: 400 });
+    }
+
+    const size = await prismaDb.size.findUnique({
+      where: {
+        id: params.sizeId,
+      },
+    });
+    return NextResponse.json(size);
+  } catch (error) {
+    console.log("[SIZE", error);
+    return new NextResponse("Internal erro", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { sizeId: string; storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { name, value } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthenticates", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+    // TODO validate image url format
+    if (!value) {
+      return new NextResponse("Value is required", { status: 400 });
+    }
+
+    if (!params.sizeId) {
+      return new NextResponse("Size Id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismaDb.store.findUnique({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const size = await prismaDb.size.updateMany({
+      where: {
+        id: params.sizeId,
+      },
+      data: {
+        name,
+        value,
+      },
+    });
+    return NextResponse.json(size);
+  } catch (error) {
+    console.log("[SIZE_PATCH", error);
+    return new NextResponse("Internal erro", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { sizeId: string; storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticates", { status: 401 });
+    }
+
+    if (!params.sizeId) {
+      return new NextResponse("Size Id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismaDb.store.findUnique({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const size = await prismaDb.size.delete({
+      where: {
+        id: params.sizeId,
+      },
+    });
+
+    return NextResponse.json(size);
+  } catch (error) {
+    console.log("[SIZE_DELETE", error);
+    return new NextResponse("Internal erro", { status: 500 });
+  }
+}
