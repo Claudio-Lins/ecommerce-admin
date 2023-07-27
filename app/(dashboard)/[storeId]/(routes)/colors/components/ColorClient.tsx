122db13f5352a8e88bf3f26f7e7929ff449a39a8
Claudio Lins <claudio.lins@me.com>
2023-07-27 09:48:40 +0100

    06:19 => create Colors

---
+++ app/(dashboard)/[storeId]/(routes)/colors/components/ColorClient.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { ColorColumn, columns } from "./Columns";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+
+interface ColorClientProps {
+  data: ColorColumn[];
+}
+
+export function ColorClient({ data }: ColorClientProps) {
+  const router = useRouter();
+  const params = useParams();
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Colors (${data.length})`}
+          description="Manage colors for you store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/colors/new`)}
+          className="flex items-center"
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add New!
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading title="API" description="API Calls for Colors" />
+      <Separator />
+      <ApiList entityName="colors" entityIdName="colorId" />
+    </>
+  );
+}
